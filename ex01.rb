bfeed8bed3b7cba39b4caf9dba6c69ffe9e8978f
eballhysa <ballhysa@hotmail.com>
2022-10-08 22:23:20 +0200

    data files added

--- ex01.rb
+++ ex01.rb
@@ -5,12 +5,10 @@ birth_year = 1995
 flag = false
 str = 'string with single quotes'
 
+
 name_birthdate = "#{name} was born on #{birth_year}"
 
-puts name_birthdate
-puts '#{name} was born on #{birth_year}'
-
-
+puts(name_birthdate)
 puts name
 puts birth_year
 puts flag
@@ -25,14 +23,17 @@ puts "Time now is #{now}"
 age = now.year - birth_year
 puts "#{name} is #{age} years old!"
 
-require 'date'  # like the Java import
 SECONDS_IN_MIN = 60 #constants
 SECONDS_IN_HOUR = 60 * SECONDS_IN_MIN
 SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR
 SECONDS_IN_YEAR = 365 * SECONDS_IN_DAY
+
+
+require 'date'  # like the Java import
 birth_date = DateTime.new 1995, 12, 21 # try without parenthesis
 exact_age = (now - birth_date.to_time) / SECONDS_IN_YEAR # difference in seconds
 puts "Exact age of #{name} is #{exact_age}!"
+
 
 puts "\n"
 
@@ -45,28 +46,26 @@ else
   puts "odd age"
 end
 
-x = 'awasdnil'
 # or equivalently
 puts age.even? ? "even age" : "odd age"
-puts "#{name} is matured" if age>20
+puts "#{name} is matured" if age>80
 
-if x then
-  puts "not printed"
-end
+
 
 
 n=5
 # loops
 puts "\n"
-n.times { |i| puts "inside loop #{i}" }
+3.times { |i| puts "inside loop #{i}" }
 3.times {puts "inside loop"}
 10.upto(20) {|i| puts "inside loop with index #{i}"}
 
+
 puts "\n"
 
-klass = nil.class #Fixnum for 22
+k = 22.class
 begin
-  puts klass;
-  klass = klass.superclass
-end while klass != nil
+  puts k;
+  k = k.superclass
+end while k != nil
 
